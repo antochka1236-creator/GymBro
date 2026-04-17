67fe382a28fc798852a232ca82658752e57a0d3e
Anton <antochka1236@gmail.com>
2026-04-17 20:01:14 +0200

    Adding code to JavaScript

--- js/script.js
+++ js/script.js
@@ -7,3 +7,31 @@ formStart.addEventListener('submit', (e) =>{
     window.location.href = "login.html"
 })
 }
+
+const formName = document.getElementById('name-form')
+const nameInput = document.getElementById('nombre')
+
+if (formName){
+    formName.addEventListener('submit', (e) =>{
+        e.preventDefault()
+
+      ifUserExist(nameInput.value)
+    })
+}
+
+const regexp  =  /^[a-zA-ZÁ-ÿ\s]{2,20}$/
+
+
+const errorName = document.getElementById('error-name')
+
+// Funcion para validar nombre
+function isNameValid(input){
+     if (regexp.test(input)){
+         errorName.textContent = ''
+         return true
+     }else{
+        errorName.textContent = 'Introduce un nombre válido (solo letras y espacios, 2-20 caracteres)'
+        return false
+     }
+}
+
