19412cfba1f1f35ac11c369138c86446d5e982dc
Anton <antochka1236@gmail.com>
2026-04-17 17:50:35 +0200

    Completed design de interface

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const formStart = document.getElementById('start-form')
 
+if (formStart){
 formStart.addEventListener('submit', (e) =>{
     e.preventDefault()
 
     window.location.href = "login.html"
 })
+}
