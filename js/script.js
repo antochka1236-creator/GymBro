4c3c59f55d74efd631d77c9286132ea3c635ee66
Anton <antochka1236@gmail.com>
2026-04-24 20:02:22 +0200

    Added JavaScript for incremetion of weights and little CSS changes

--- js/script.js
+++ js/script.js
@@ -152,6 +152,8 @@ fetch('programs.json')
    
   .catch((err)=> console.log('Solicitud fallida', err))
 
+
+const weights = JSON.parse(localStorage.getItem('weights')) || {}
 // Funcion para renderizar la pagina de workout
 function generateProgram(data){
   
@@ -171,26 +173,21 @@ function generateProgram(data){
     btn.classList.add('save-weight')
     btn.textContent = 'Guardar pesos'
 
+    const errorPesos = document.createElement('span')
+    errorPesos.classList.add('error', 'hidden')
+
     
-    
-    day.exercises.forEach(ex =>{
-      const exersicesContainer = document.createElement('div')
-      exersicesContainer.innerHTML = ` <h3>${ex.name}</h3>
-                    <p>${ex.sets} sets x ${ex.rep} reps</p>
-                    <input type="text" placeholder="Introduce el peso(Kg)" class="weight-input" id="input-${day.day}-${ex.name.replace(/\s/g, '-')}">
-                    <span class="error hidden"></span>
-                  `
-    card.appendChild(exersicesContainer)
-    })
+    renderExersices(card, day, weights)
 
     card.appendChild(btn)
+    card.appendChild(errorPesos)
     fragment.appendChild(card)
 
     btn.addEventListener('click', () =>{
-        const weights = JSON.parse(localStorage.getItem('weights')) || {}
-        
+       
+      let allValid = true
 
-        document.querySelectorAll('.weight-input').forEach(input =>{
+      card.querySelectorAll('.weight-input').forEach(input =>{
           
           const errorMsg = input.nextElementSibling
 
@@ -199,14 +196,22 @@ function generateProgram(data){
           if (!regexWeight.test(input.value)){
             errorMsg.textContent = 'Solo numeros (máx. 3 dígitos)'
             errorMsg.classList.remove('hidden')
+            errorPesos.textContent = 'Revisa los pesos introducidos antes de guardar'
+            errorPesos.classList.remove('hidden')
+            allValid = false
             return
           }
+          
+          errorPesos.classList.add('hidden')
+          errorMsg.classList.add('hidden')
+          weights[input.id] = input.value
 
-           errorMsg.classList.add('hidden')
-            weights[input.id] = input.value
         })
 
+        if (!allValid) return
+
         localStorage.setItem('weights', JSON.stringify(weights))
+        renderExersices(card, day, weights)
        
     })
   })
@@ -236,3 +241,69 @@ function createContainerFlex(){
 
 // Regex para el peso
 const regexWeight = /^\d{1,3}$|^\d{1,2}\.\d$/
+
+// Funcion para renderizar ejersicios
+function renderExersices(card, day, weights){
+  card.querySelectorAll('.exercise').forEach(ex=> ex.remove())
+
+  day.exercises.forEach(ex=>{
+    const exersicesContainer = document.createElement('div')
+    exersicesContainer.classList.add('exercise')
+    const inputId = `input-${day.day}-${ex.name.replace(/\s/g, '-')}`
+    const savedWeight = weights[inputId]
+
+    if (savedWeight){
+      exersicesContainer.innerHTML = `
+              <h3>${ex.name}</h3>
+              <p>${ex.sets} sets x ${ex.rep} reps</p>
+              <div class="weight-control">
+              <input type="text" placeholder="Kg" class="weight-input" value="${savedWeight}" id="input-${day.day}-${ex.name.replace(/\s/g, '-')}">
+                        <button class="minus">-</button>
+                        <button class="plus">+</button>
+                    </div>
+                    `
+        const btnPlus = exersicesContainer.querySelector('.plus')
+        const btnMinus = exersicesContainer.querySelector('.minus')
+        
+        btnPlus.addEventListener('click', () =>{
+              if (parseFloat(weights[inputId]) < 20){
+            weights[inputId] = parseFloat(weights[inputId]) + 1
+          }else if(parseFloat(weights[inputId]) < 60){
+              weights[inputId] = parseFloat(weights[inputId]) + 2.5
+          }else{
+            weights[inputId] = parseFloat(weights[inputId]) + 5
+          }
+
+          localStorage.setItem('weights', JSON.stringify(weights))
+          renderExersices(card, day, weights)
+        })
+
+        btnMinus.addEventListener('click', () =>{
+          if (parseFloat(weights[inputId]) < 20){
+            weights[inputId] = parseFloat(weights[inputId]) - 1
+          }else if(parseFloat(weights[inputId]) < 60){
+              weights[inputId] = parseFloat(weights[inputId]) - 2.5
+          }else{
+            weights[inputId] = parseFloat(weights[inputId]) - 5
+          }
+
+          localStorage.setItem('weights', JSON.stringify(weights))
+          renderExersices(card, day, weights)
+        })
+
+    }else{
+      exersicesContainer.innerHTML = `
+            <h3>${ex.name}</h3>
+            <p>${ex.sets} sets x ${ex.rep} reps</p>
+            <input type="text" placeholder="Introduce el peso(Kg)" class="weight-input" id="input-${day.day}-${ex.name.replace(/\s/g, '-')}">
+            <span class="error hidden"></span>`
+    }
+    
+
+    card.insertBefore(exersicesContainer, card.querySelector('.save-weight'))
+   
+  })
+  
+
+}
+
